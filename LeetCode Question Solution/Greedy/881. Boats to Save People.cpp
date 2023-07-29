b87f6b99bbee21c05776bef49596d0a39afb0682
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-07-29 18:00:37 +0530

    Create 881. Boats to Save People.cpp

---
+++ LeetCode Question Solution/Greedy/881. Boats to Save People.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n=people.size();
+        int boats=0;
+        sort(people.begin(), people.end());
+        int i=0, j=n-1;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                 i++;
+                 j--;
+            }
+            else {
+                j--;
+            }
+            boats++;
+        }
+
+        return boats;
+
+    }
+};
