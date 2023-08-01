7035f76e2a375775187e084e98184a449800a8c7
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-08-01 11:13:31 +0530

    Create Gfg Minimum sum partition.cpp

---
+++ LeetCode Question Solution/DP/DP on Subsequences | Subsets/Gfg Minimum sum partition.cpp
@@ -0,0 +1,37 @@
+class Solution{
+
+  public:
+	int minDifference(int arr[], int n)  { 
+	    // Your code goes here
+	    int totSum=0;
+	    for(int i=0; i<n; i++){
+	        totSum+=arr[i];
+	    }
+	    int k=totSum;
+	     vector<vector<bool>>dp(n, vector<bool>(k+1, 0));
+         for(int i=0; i<n; i++){
+           dp[i][0]=1;
+           }
+        if(arr[0]<=k){
+         dp[0][arr[0]]=1;
+           }
+
+         for(int i=1; i<n; i++){
+         for(int target=1; target<=k; target++){
+            bool ntake=dp[i-1][target];
+            bool take=0;
+            if(target>=arr[i]) take=dp[i-1][target-arr[i]];
+            dp[i][target]=(take || ntake);
+         }
+       }
+       
+       int mini=1e9;
+       for(int s1=0; s1<=k/2; s1++){
+           if(dp[n-1][s1]==1){
+               mini=min(mini, abs((k-s1)-s1));
+           }
+       }
+       return mini;
+	}
+};
+
