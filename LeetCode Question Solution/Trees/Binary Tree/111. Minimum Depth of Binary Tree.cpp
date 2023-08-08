710d0a1b6f5324e86353deba457c891ef9c8975d
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-08-08 19:25:55 +0530

    Create 111. Minimum Depth of Binary Tree.cpp

---
+++ LeetCode Question Solution/Trees/Binary Tree/111. Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root==NULL) return 0;
+        int left=minDepth(root->left);
+        int right=minDepth(root->right);
+        if(left==0)return 1+right;
+        else if(right==0) return 1+left;
+        else return 1+ min(left,right);
+    }
+};
