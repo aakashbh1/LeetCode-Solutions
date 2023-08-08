a6ba305f80bdc2cfca744fb05dbd997ad02e0727
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-08-08 19:40:50 +0530

    Create 12. Integer to Roman.cpp

---
+++ LeetCode Question Solution/Strings/12. Integer to Roman.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<int>    val{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    vector<string> sym{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+  
+  
+        
+        string result = "";
+        int i = 0;
+        while(num > 0) {
+            while(num >= val[i]) {
+                num -= val[i];
+                result += sym[i];
+            }
+            i++;
+        }
+        return result;
+        
+    }
+    
+};
