df0bad866bac7074f526544d38739214e59c8a23
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-08-16 19:59:34 +0530

    Create 343. Integer Break.cpp

---
+++ LeetCode Question Solution/DP/Normal Dp /343. Integer Break.cpp
@@ -0,0 +1,49 @@
+Memoization
+
+class Solution {
+private:
+    int solve(int n,vector<int>& dp){
+        if(n==0 || n==1) return 1;
+        
+        if(dp[n]!=-1) return dp[n];
+        
+        int maxi=0;
+        
+        for(int ind=1;ind<n;ind++){
+            int temp=max(ind*(n-ind), ind * solve(n-ind,dp));
+            maxi=max(maxi,temp);
+        }
+        return dp[n]=maxi;
+    }
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
+Tabulation
+
+class Solution {
+private:
+    int solveTab(int n){
+        vector<int> dp(n+1,0);
+        
+        dp[0]=1;
+        dp[1]=1;
+     
+        for(int i=2;i<=n;i++){
+            int maxi=1;
+            for(int ind=1;ind<i;ind++){
+            int temp=max(ind*(i-ind), ind * dp[i-ind]);
+            maxi=max(maxi,temp);
+            }
+            dp[i]=maxi;
+        }
+        
+        return dp[n];
+    }
+public:
+    int integerBreak(int n) {
+        return solveTab(n);
+    }
+};
