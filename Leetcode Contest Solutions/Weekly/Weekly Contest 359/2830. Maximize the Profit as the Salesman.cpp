783c3ad955a4819a9fa4efae0e9d59b3784565ac
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-08-21 10:38:00 +0530

    Create 2830. Maximize the Profit as the Salesman.cpp

---
+++ Leetcode Contest Solutions/Weekly/Weekly Contest 359/2830. Maximize the Profit as the Salesman.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    int solve(int idx, vector<vector<int>> &offers, vector<int> &dp) {
+        
+        if (idx == offers.size()) return 0;
+        if (dp[idx] != -1) return dp[idx];
+        
+        int notTake = solve(idx+1, offers, dp);
+        
+        int low = idx+1, high = offers.size() - 1;
+        int next_idx = offers.size();
+        
+        // finding the idx for the next offer if we take idx as the current offer
+        while (low <= high) {
+            int mid = (low+high)/2;
+            if (offers[mid][0] > offers[idx][1]) {
+                high = mid - 1;
+                next_idx = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+      //using Upper Bound
+      // int nextIndex=upper_bound(st.begin(),st.end(),val)-st.begin();
+
+        // peforming this & use binary search to find the next idx to perform;
+        int take = offers[idx][2] + solve(next_idx, offers, dp);
+        return dp[idx] = max(take, notTake);
+    }
+public:
+    int maximizeTheProfit(int n, vector<vector<int>> &offers) {
+        int m = offers.size();
+        sort(offers.begin(), offers.end());
+
+        vector<int> dp(m, -1);
+        return solve(0, offers, dp);
+    }
+};
