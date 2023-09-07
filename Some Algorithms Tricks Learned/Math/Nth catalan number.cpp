5407a1845b3499d03032f64dd159528c86eb0056
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-09-07 12:34:27 +0530

    Update Nth catalan number.cpp

--- Some Algorithms Tricks Learned/Math/Nth catalan number.cpp
+++ Some Algorithms Tricks Learned/Math/Nth catalan number.cpp
@@ -1,3 +1,4 @@
+//Application Below
 class Solution
 {
     public:
@@ -38,3 +39,28 @@ class Solution
     return solveTab(n);
     }
 };
+
+
+Parentheses Expressions: The nth Catalan number represents the number of different ways to arrange parentheses in a valid expression.
+This is crucial in problems related to well-formed parentheses, like checking the validity of expressions and generating all valid combinations.
+
+Binary Search Trees (BSTs): The nth Catalan number gives the number of unique BSTs that can be formed with n nodes. 
+This concept is applied in optimizing search operations and data retrieval.
+
+Dyck Paths: Catalan numbers also count the number of paths that stay above the diagonal in a grid from (0,0) to (n,n), but do not cross it.
+These paths have steps (1,1) or (1,-1) which are often referred to as "up" and "down" steps.
+
+Triangulations of a Polygon: The nth Catalan number also represents the number of ways to triangulate a polygon with n+2 sides. 
+This is essential in various geometric algorithms.
+
+Non-crossing Partitions: In combinatorics, Catalan numbers count the number of non-crossing partitions of a set of n elements. 
+This concept is important in solving problems related to discrete structures.
+
+Expression Trees: Catalan numbers are used in the calculation of the number of different ways to construct a full binary tree with n+1 leaves. 
+This is applicable in compiler design and expression evaluation.
+
+Balanced Ternary Strings: Catalan numbers count the number of balanced ternary strings of length 2n, 
+where a balanced ternary string is one in which the number of 1s is equal to the number of -1s.
+
+Counting Paths in a Grid: In a grid with n rows and n columns, the nth Catalan number gives the number of paths 
+from the top-left corner to the bottom-right corner that do not cross the diagonal.
