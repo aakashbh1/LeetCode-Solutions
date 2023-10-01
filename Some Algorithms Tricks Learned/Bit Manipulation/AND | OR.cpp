d958d87b8b17c6b389c9be39aace64f203e24ee0
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-10-01 10:05:12 +0530

    Create  AND | OR.cpp

---
+++ Some Algorithms Tricks Learned/Bit Manipulation/AND | OR.cpp
@@ -0,0 +1,7 @@
+//------------------------------AND-----------------------------------
+
+1. And of two numbers result in a number less than or equal to the minimum of the two.
+  example: 5 and 7 is 5   3 and 5 is 1 
+
+
+//------------------------------OR--------------------------------------------
