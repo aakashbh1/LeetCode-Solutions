6a98aa38b314ee3dd750d6b5d5b6d93b1052e45c
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-10-07 18:38:25 +0530

    Update 22. Generate Parentheses.cpp

--- Patterns/BackTracking/Khandani Template/22. Generate Parentheses.cpp
+++ Patterns/BackTracking/Khandani Template/22. Generate Parentheses.cpp
@@ -5,8 +5,8 @@ public:
         if(tmp.length()==2*n) {res.push_back(tmp); return;}
         if(open<n){
             tmp.push_back('(');
-            util(res,open+1,close,tmp,n);
-            tmp.pop_back();
+            util(res,open+1,close,tmp,n); //if(open<n)generate(open+1,close,n,s+"("); works the same as we add it in the function call so it automatically
+            tmp.pop_back();							// gets removed(pop) whenever function backtracks
         }
         if(close<open){
             tmp.push_back(')');
@@ -22,5 +22,18 @@ public:
         string temp="";
         util(res,open,close,temp,n);
         return res;
+    }
+};
+class Solution {
+public:
+vector<string>ans;
+   void generate (int open,int close ,int n,string s){
+       if(s.size()==2*n)ans.push_back(s);
+       if(open<n)generate(open+1,close,n,s+"(");
+       if(close<open)generate(open,close+1,n,s+")");
+   }
+    vector<string> generateParenthesis(int n) {
+        generate(0,0,n,"");
+        return ans;
     }
 };
