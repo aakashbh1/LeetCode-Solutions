74961839af0131aaf497204d5b9c257ebe958516
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-10-15 11:22:44 +0530

    Create 2903. Find Indices With Index and Value Difference I.cpp

---
+++ Leetcode Contest Solutions/Weekly/Weekly Contest 367/2903. Find Indices With Index and Value Difference I.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+        
+        
+        vector<int> ans={-1,-1};
+        
+        for(int i=0;i<nums.size();i++){
+            for(int j=i;j<nums.size();j++){
+                if(abs(i - j) >= indexDifference && abs(nums[i] - nums[j]) >= valueDifference){
+                    ans[0]=i;
+                    ans[1]=j;
+                    break;
+                    
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
